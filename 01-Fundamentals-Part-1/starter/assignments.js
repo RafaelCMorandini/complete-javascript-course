f9b463bf8a7ff26679215495eca036fffd40764f
RafaelCMorandini <rafaelmorandini200@gmail.com>
2025-01-28 15:10:22 -0300

    Lecture 13
    Finished Lecture 13 and associated exercise

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -21,3 +21,24 @@ console.log(typeof population);
 // let, const and var
 
 language = 'Portuguese';
+
+//Basic Operators
+
+console.log(population / 2);
+
+population++;
+
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+const description1 =
+    country +
+    ' is in ' +
+    continent +
+    ', and its ' +
+    population +
+    ' million people speak ' +
+    language;
+
+console.log(description1);
