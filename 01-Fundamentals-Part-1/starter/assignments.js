d84f60ac39b3e564e79687c1485e9950d8bc6fba
RafaelCMorandini <rafaelmorandini200@gmail.com>
2025-01-28 15:43:30 -0300

    Lecture 14, 15, 16 and 17
    Finished Lecture 14 through 17 and its associated exercise and coding challenge

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -3,26 +3,26 @@
 const country = 'Brazil';
 const continent = 'South America';
 let population = '220';
-/*
+
 console.log(country);
 console.log(continent);
 console.log(population);
-*/
+
 // Data Types
 
 const isIsland = false;
 let language;
-/*
+
 console.log(typeof isIsland);
 console.log(typeof country);
 console.log(typeof continent);
 console.log(typeof population);
-*/
+
 // let, const and var
 
 language = 'Portuguese';
 
-//Basic Operators
+// Basic Operators
 
 console.log(population / 2);
 
@@ -42,3 +42,9 @@ const description1 =
     language;
 
 console.log(description1);
+
+// Strings and Template Literals
+
+const description2 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+
+console.log(description2);
