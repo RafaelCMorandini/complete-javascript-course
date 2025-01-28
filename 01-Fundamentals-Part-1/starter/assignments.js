682a8a3d2a98621b00fe10c15bafd62e3cfb94a1
RafaelCMorandini <rafaelmorandini200@gmail.com>
2025-01-28 14:40:14 -0300

    Lecture 11
    Finished Lecture 11 and related exercise

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,8 +1,22 @@
 // Values and Variables
+
 let country = 'Brazil';
 let continent = 'South America';
 let population = '220';
-
+/*
 console.log(country);
 console.log(continent);
 console.log(population);
+*/
+// Data Types
+
+let isIsland = false;
+let language;
+/*
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+*/
+// let, const and var
+
