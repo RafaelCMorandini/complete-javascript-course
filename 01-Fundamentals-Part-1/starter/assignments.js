f16bba6ba7e20c4c8c9008a7f37ff768059079bc
RafaelCMorandini <rafaelmorandini200@gmail.com>
2025-01-28 15:59:57 -0300

    Lecture 18 and 19
    Finished Lecture 18, 19 and associated exercise and coding challenge

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -48,3 +48,11 @@ console.log(description1);
 const description2 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 
 console.log(description2);
+
+// Taking Decisions: if / else Statements
+
+if (population > 33) {
+    console.log(`${country}'s population is above average`);
+} else {
+    console.log(`${country}'s population is ${33 - population} million below average`);
+}
