d8f6f8311826478df0a8d545ea99f704accf9502
RafaelCMorandini <rafaelmorandini200@gmail.com>
2025-01-29 13:08:46 -0300

    Lecture 39
    Finished Lecture 39 and associated exercise

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -63,3 +63,18 @@ const describePopulation = function (country, population) {
 console.log(describePopulation('Brazil', 216));
 console.log(describePopulation('China', 1441));
 console.log(describePopulation('USA', 332));
+
+//Introduction to Arrays
+
+const populations = [216, 1441, 332, 83];
+
+console.log(populations.length === 4);
+
+const percentages = [
+    percentageOfWorld1(populations[0]),
+    percentageOfWorld1(populations[1]),
+    percentageOfWorld1(populations[2]),
+    percentageOfWorld1(populations[3])
+];
+
+console.log(percentages);
