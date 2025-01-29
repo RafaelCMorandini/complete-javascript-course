432254f9921f350e9a14758681e599d70c763474
RafaelCMorandini <rafaelmorandini200@gmail.com>
2025-01-28 22:51:04 -0300

    Lecture 34
    Finished Lecture 34 and associated exercise

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -13,3 +13,30 @@ const descBrazil = describeCountry('Brazil', 216, 'Brasilia');
 console.log(descPortugal);
 console.log(descGermany);
 console.log(descBrazil);
+
+// Function Declarations vs. Expressions
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100
+}
+
+const percentageOfWorld2 = function (population) {
+    return (population / 7900) * 100
+}
+
+
+const percentageChina1 = percentageOfWorld1(1441);
+const percentageGermany1 = percentageOfWorld1(83);
+const percentageBrazil1 = percentageOfWorld1(216);
+
+console.log(percentageChina1);
+console.log(percentageGermany1);
+console.log(percentageBrazil1);
+
+const percentageChina2 = percentageOfWorld2(1441);
+const percentageGermany2 = percentageOfWorld2(83);
+const percentageBrazil2 = percentageOfWorld2(216);
+
+console.log(percentageChina2);
+console.log(percentageGermany2);
+console.log(percentageBrazil2);
